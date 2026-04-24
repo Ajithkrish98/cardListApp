e317b07a99e9a213d30044a4a437d506bf525dbe
Ajith <63662118+Ajithkrish98@users.noreply.github.com>
2026-04-24 07:14:07 +0530

    CardList API Call with FlatList

--- App.tsx
+++ App.tsx
@@ -1,45 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  Image,
+  StatusBar,
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+  // Fake API Call
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+      );
+      const result = await response.json();
+      setData(result);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+  const renderItem = ({ item }: any) => (
+    <View style={styles.card}>
+      <Image
+        source={{
+          uri: `https://i.pravatar.cc/150?img=${item.id}`,
+        }}
+        style={styles.image}
       />
+
+      <View style={{ flex: 1 }}>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.email}>{item.email}</Text>
+        <Text style={styles.company}>{item.company.name}</Text>
+      </View>
     </View>
   );
-}
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+      <Text style={styles.header}>Users List</Text>
+
+      {loading ? (
+        <ActivityIndicator size="large" style={styles.loader}/>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item: any) => item.id.toString()}
+          renderItem={renderItem}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  card: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+    elevation: 3,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 15,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  email: {
+    color: 'gray',
+    marginTop: 5,
+  },
+  company: {
+    color: '#333',
+    marginTop: 5,
+  },
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color:'blue'
   },
 });
-
-export default App;
